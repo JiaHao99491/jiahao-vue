86d37107d6947ad34745f62cfe283cbf679c5be1
JiaHao99491 <liujiahao99491@gmail.com>
2025-03-13 11:18:52 +0800

    docs: 文档样式重置

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -2,8 +2,11 @@
 import { h, defineComponent, onMounted } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import 'jiahao-vue/lib.css'
+
 import './style.css'
+import 'jiahao-vue/lib.css'
+import './reset.css'
+
 import { initThemeSwitcher } from './theme-switcher-support'
 
 export default {
